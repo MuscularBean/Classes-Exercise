24e70b6eb993c2f8e6d5264e75208f55e29d1f37
MuscuarBean <email@example.com>
2023-11-18 03:52:24 -0700

    Exercise Complete

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Classes
+﻿using Microsoft.VisualBasic;
+using System.Reflection;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var Car = new Car()
+            { 
+                Make = "Scion",
+                Model = "tC",
+                Year = 2008,
+
+            };
+
+            Console.WriteLine($"My car is a {Car.Year} {Car.Make} {Car.Model}");
+           
+
         }
     }
 }
